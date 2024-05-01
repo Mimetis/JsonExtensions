824b9c91be321c5e2c4a6a2e56563bd1d627fc8e
Edward Miller <edward.miller@mesirow.com>
2024-05-01 13:04:09 -0500

    fix netstandard

--- JsonExtensions/JsonReader.cs
+++ JsonExtensions/JsonReader.cs
@@ -270,7 +270,11 @@ namespace JsonExtensions
             if (this.TokenType != JsonTokenType.PropertyName && this.TokenType != JsonTokenType.String)
                 return null;
 
+#if NET6_0_OR_GREATER
             var str = utf8Encoding.GetString(this.Value.Span);
+#else   
+            var str = utf8Encoding.GetString(this.Value.ToArray());
+#endif
 
             return Regex.Unescape(str);
         }
@@ -286,7 +290,11 @@ namespace JsonExtensions
             if (this.TokenType != JsonTokenType.PropertyName && this.TokenType != JsonTokenType.String)
                 return null;
 
+#if NET6_0_OR_GREATER
             var str = utf8Encoding.GetString(this.Value.Span);
+#else   
+            var str = utf8Encoding.GetString(this.Value.ToArray());
+#endif
 
             return str;
         }
