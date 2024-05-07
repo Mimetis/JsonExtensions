edf5ad0fcf05b010daaebaadbc5a2c990bee295c
Edward Miller <edward.miller@mesirow.com>
2024-05-07 13:06:26 -0500

    improve benchmarks

--- Benchmarks/JsonReaderBenchmarks.cs
+++ Benchmarks/JsonReaderBenchmarks.cs
@@ -10,6 +10,8 @@ public class JsonReaderBenchmarks
     private JsonReader? _jsonReader;
     private FileStream? _fileStream;
 
+    private const int IterationsNum = 100;
+
     [GlobalSetup]
     public void Setup()
     {
@@ -22,36 +24,61 @@ public class JsonReaderBenchmarks
     public void Cleanup()
     {
         _fileStream?.Dispose();
+        _jsonReader?.Dispose();
+    }
+
+    [IterationCleanup]
+    public void IterationCleanup()
+    {
+        _fileStream?.Seek(0, SeekOrigin.Begin);
+        _jsonReader?.Dispose();
+        _jsonReader = new JsonReader(_fileStream);
     }
 
     [Benchmark]
+    [IterationCount(IterationsNum)]
     public void Read()
     {
         ArgumentNullException.ThrowIfNull(_jsonReader);
 
+        int count = 0;
+
         while (_jsonReader.Read())
         {
+            var tokenString = _jsonReader.GetString();
+
             if (_jsonReader.TokenType == JsonTokenType.PropertyName)
             {
-                // Perform some operation
-                var propertyName = _jsonReader.GetString();
+                // TODO: Perform some operation
             }
+
+            count++;
         }
+
+        Console.WriteLine("Count: " + count);
     }
 
     
     [Benchmark]
+    [IterationCount(IterationsNum)]
     public void Values()
     {
         ArgumentNullException.ThrowIfNull(_jsonReader);
 
+        int count = 0;
+
         foreach (var value in _jsonReader.Values())
         {
+            var tokenString = value.ToString();
+
             if (value.TokenType == JsonTokenType.PropertyName)
             {
-                var tokenString = value.ToString();
-                Console.WriteLine(tokenString);
+                // TODO: Perform some operation
             }
+
+            count++;
         }
+
+        Console.WriteLine("Count: " + count);
     }
 }
