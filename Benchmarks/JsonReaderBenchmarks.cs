3fbe706fbd066b3db34ae6871680ffedfeaa84d7
Edward Miller <symbiogenisis@outlook.com>
2024-06-29 13:13:58 -0500

    add comparisons

--- Benchmarks/JsonReaderBenchmarks.cs
+++ Benchmarks/JsonReaderBenchmarks.cs
@@ -2,13 +2,18 @@ namespace JsonExtensions.Benchmarks;
 
 using BenchmarkDotNet.Attributes;
 using System.Text.Json;
+using System.Text.Json.Stream;
+using Wololo.Text.Json;
 
 [MemoryDiagnoser]
 [Config(typeof(AntiVirusFriendlyConfig))]
 public class JsonReaderBenchmarks
 {
     private JsonReader? _jsonReader;
-    private FileStream? _fileStream;
+    private Utf8JsonStreamReader? _jsonStreamReader;
+    private Utf8JsonAsyncStreamReader? _jsonStreamAsyncReader;
+
+    private FileStream _fileStream = null!;
 
     private const int IterationsNum = 100;
 
@@ -16,28 +21,30 @@ public class JsonReaderBenchmarks
     public void Setup()
     {
         string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dummy-data.json");
-        _fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
-        _jsonReader = new JsonReader(_fileStream);
+        _fileStream = new(path, FileMode.Open, FileAccess.Read);
+        _jsonReader = new(_fileStream);
+        _jsonStreamReader = new();
+        _jsonStreamAsyncReader = new(_fileStream);
     }
 
     [GlobalCleanup]
     public void Cleanup()
     {
-        _fileStream?.Dispose();
+        _fileStream.Dispose();
         _jsonReader?.Dispose();
     }
 
     [IterationCleanup]
     public void IterationCleanup()
     {
-        _fileStream?.Seek(0, SeekOrigin.Begin);
+        _fileStream.Seek(0, SeekOrigin.Begin);
         _jsonReader?.Dispose();
         _jsonReader = new JsonReader(_fileStream);
     }
 
     [Benchmark]
     [IterationCount(IterationsNum)]
-    public void Read()
+    public void ReadMimetis()
     {
         ArgumentNullException.ThrowIfNull(_jsonReader);
 
@@ -45,11 +52,12 @@ public class JsonReaderBenchmarks
 
         while (_jsonReader.Read())
         {
-            var tokenString = _jsonReader.GetString();
-
-            if (_jsonReader.TokenType == JsonTokenType.PropertyName)
+            switch (_jsonReader.TokenType)
             {
-                // TODO: Perform some operation
+                case JsonTokenType.PropertyName:
+                case JsonTokenType.String:
+                    var tokenString = _jsonReader.GetString();
+                    break;
             }
 
             count++;
@@ -58,10 +66,9 @@ public class JsonReaderBenchmarks
         Console.WriteLine("Count: " + count);
     }
 
-    
     [Benchmark]
     [IterationCount(IterationsNum)]
-    public void Values()
+    public void ValuesMimetis()
     {
         ArgumentNullException.ThrowIfNull(_jsonReader);
 
@@ -73,7 +80,57 @@ public class JsonReaderBenchmarks
 
             if (value.TokenType == JsonTokenType.PropertyName)
             {
-                // TODO: Perform some operation
+                // Console.WriteLine("PropertyName Found");
+            }
+
+            count++;
+        }
+
+        Console.WriteLine("Count: " + count);
+    }
+
+    [Benchmark]
+    [IterationCount(IterationsNum)]
+    public async Task ReadHarrtell()
+    {
+        ArgumentNullException.ThrowIfNull(_jsonStreamReader);
+
+        int count = 0;
+
+        void CountTokens(ref Utf8JsonReader reader)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.PropertyName:
+                case JsonTokenType.String:
+                    var tokenString = reader.GetString();
+                    break;
+            }
+
+            count++;
+        }
+
+        await _jsonStreamReader.ReadAsync(_fileStream, CountTokens);
+
+        Console.WriteLine("Count: " + count);
+    }
+
+    [Benchmark]
+    [IterationCount(IterationsNum)]
+    public async Task ReadGraGra()
+    {
+        ArgumentNullException.ThrowIfNull(_jsonStreamAsyncReader);
+
+        int count = 0;
+
+        while (await _jsonStreamAsyncReader.ReadAsync())
+        {
+            switch (_jsonStreamAsyncReader.TokenType)
+            {
+                case JsonTokenType.PropertyName:
+                case JsonTokenType.String:
+                    var tokenString = _jsonStreamAsyncReader.GetString();
+                    break;
             }
 
             count++;
