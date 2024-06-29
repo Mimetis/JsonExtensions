0dd4957d86562ceceb215d50131a21f7048b2672
Edward Miller <symbiogenisis@outlook.com>
2024-06-29 13:40:18 -0500

    add debugging

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,11 +1,16 @@
 ﻿namespace JsonExtensions.Benchmarks;
 
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 public class Program
 {
+#if DEBUG
+    static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+#else
     static void Main(string[] args)
     {
         _ = BenchmarkRunner.Run<JsonReaderBenchmarks>();
     }
+#endif
 }
